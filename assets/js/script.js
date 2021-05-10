08044fbc3e69e587022ddf721136f4310b292235
Tima Nlemvo <tnlemvo@AML-TNLEMVO.caa.comgit commit -m added>
2021-05-10 08:36:49 -0700

    re-did the script page.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,69 @@
 // Assignment code here
+function generatePassword(){
 
-var lowercaseCharacter = "abcdefghijklmnopqrstuvwxyz";
-var uppercaseCharacter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numberCharacter = "1234567890";
-var specialCharater = "~!@#$%^&*()_+`{}|[]\;':,./<>?";
-
-
-function generatePassword (){
-
-    var userSelection = []
-
-    var lowercaseCharacter = confirm("Will it require lowercase letters?");
-    var uppercaseCharacter = confirm("Will it require uppercase letters?");
-    var numberCharacter = confirm("Will it require numbers?");
-    var specialCharater = confirm("will it require special characters?");
-
-    if ((lowercaseCharacter===false) && (uppercaseCharacter===false) && (numberCharacter===false) && (specialCharater===false))
-
-    var passwordLength = prompt ("What is the password length need? Min: 8 Max: 128")
-
-    if ((passwordLength < 8) || (passwordLength > 128)){
-        alert("Please choose password length.")
+    var userPick = [];
+    
+    alert('Confirm All Conditions')
+    var upper = confirm("Use uppercase letters?");
+    var lower = confirm("Use lowercase letters?");
+    var specialC = confirm("Use special characters?");
+    var number = confirm("Use numbers?");
+    
+    
+    if ((upper===false) && (lower===false) && (specialC===false) && (number===false)) {
+      alert ('Need to Confirm Condition');
+      var upper = confirm("Use uppercase letters?");
+      var lower = confirm("Use at least one lowercase letter");
+      var specialC = confirm("Use at least special character");
+      var number = confirm("Use at least one number");
+      }
+    
+      // password length
+      var passwordAmount = prompt ("How long would you like your password? Please choose between 8-128.");
+      if ((passwordAmount < 8) || (passwordAmount > 128)){
+        alert("Please choose your password length. Must be between 8-128.");
+        passwordAmount = prompt("Choose Length");
+      } 
+    
+   
+    var upperCase= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var lowerCase= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var numbers = ['1','2','3','4','5','6','7','8','9'];
+    var special = ["\"","!","#","$","%","&","\'","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","]","^","`","{","|","}","~"];
+    
+    
+    if (upper) {
+      for(var i = 0; i < upperCase.length; i++ ){
+        userPick.push(upperCase[i]);
+    
+      }
     }
-
-    if (lowercaseCharacter) {
-        for(var i = 0; i < lowercaseCharacter.length; i++)
-        userSelection.push(lowercaseCharacter[i])
+    
+    if(lower){
+      for(var i=0; i < lowerCase.length; i++){
+        userPick.push(lowerCase[i]);
+      }
     }
-
-    if (uppercaseCharacter) {
-        for(var i = 0; i < uppercaseCharacter.length; i++)
-        userSelection.push(uppercaseCharacter[i])
+    
+    if(special){
+      for(var i=0; i < special.length; i++){
+        userPick.push(special[i]);
+      }
     }
-
-    if (numberCharacter) {
-        for(var i = 0; i < numberCharacter.length; i++)
-        userSelection.push(numberCharacter[i])
+    
+    if(numbers){
+      for(var i=0; i < numbers.length; i++){
+        userPick.push(numbers[i]);
+      }
     }
-
-    if (specialCharacter) {
-        for(var i = 0; i < specialCharacter.length; i++)
-        userSelection.push(specialCharacter[i])
+    
+    var finalResult = [];
+    for(i=0; i < passwordAmount; i++){
+      finalResult.push(userPick[Math.floor(Math.random()* userPick.length)]);
     }
-
-    var passwordResult = [];
-    for(i=0; i <passwordLength; i++){
-        passwordLength.push(userSelection[Math.floor(Math.random()* userSelection.length)]);
+    // return finalResult
+    return(finalResult.join(""));
     }
-
-    return(passwordResult.join(""));
-}
 
 
 // Get references to the #generate element
